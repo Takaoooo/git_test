1fc03cdd012b8512bc1f970a6b70d1a57b97f15c
Takaoooo <takao_lotte7@yahoo.co.jp>
2018-11-29 18:13:12 +0900

    first test

---
+++ script.sh
@@ -0,0 +1,10 @@
+conda info
+conda config -append channels conda-fprge
+conda create --name Takaoooo python=3.6 -y
+source activate Takaoooo
+conda install -c menpo opencv3 -y
+pip3 install opencv-python
+conda install -c anaconda numpy -y 
+conda install pandas -y 
+conda install -c anaconda sqlalchemy -y
+conda install -c conda- forge matplotlib -y
